12692606be2bb696e7d3331747ffdbfcee353e0d
ztisdashen <648742668@qq.com>
2021-07-10 22:26:10 +0800

    修改了mock的网络 后端网络为http://localhost:9999

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -31,24 +31,57 @@ const mutations = {
 const actions = {
   // user login
   login({ commit }, userInfo) {
-    const { username, password } = userInfo
+    // const { username, password } = userInfo
+    // return new Promise((resolve, reject) => {
+    //   login({ username: username.trim(), password: password }).then(response => {
+    //     const { data } = response
+    //     commit('SET_TOKEN', data.token)
+    //     setToken(data.token)
+    //     resolve()
+    //   }).catch(error => {
+    //     reject(error)
+    //   })
+    // })
     return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
-        const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
+        commit('SET_TOKEN', 'admin-token')
+        setToken('admin-token')
         resolve()
-      }).catch(error => {
-        reject(error)
-      })
     })
   },
 
   // get user info
   getInfo({ commit, state }) {
+    // return new Promise((resolve, reject) => {
+    //   getInfo(state.token).then(response => {
+    //     const { data } = response
+    //
+    //     if (!data) {
+    //       reject('Verification failed, please Login again.')
+    //     }
+    //
+    //     const { roles, name, avatar, introduction } = data
+    //
+    //     // roles must be a non-empty array
+    //     if (!roles || roles.length <= 0) {
+    //       reject('getInfo: roles must be a non-null array!')
+    //     }
+    //
+    //     commit('SET_ROLES', roles)
+    //     commit('SET_NAME', name)
+    //     commit('SET_AVATAR', avatar)
+    //     commit('SET_INTRODUCTION', introduction)
+    //     resolve(data)
+    //   }).catch(error => {
+    //     reject(error)
+    //   })
+    // })
     return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
-        const { data } = response
+        const  data  = {
+          roles: ['admin'],
+          introduction: 'I am a super administrator',
+          avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+          name: 'Super Admin'
+        }
 
         if (!data) {
           reject('Verification failed, please Login again.')
@@ -66,9 +99,6 @@ const actions = {
         commit('SET_AVATAR', avatar)
         commit('SET_INTRODUCTION', introduction)
         resolve(data)
-      }).catch(error => {
-        reject(error)
-      })
     })
   },
 
